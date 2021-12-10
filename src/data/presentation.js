015a108b8d1710f813954564b93ccc7c33655815
qmenoret <quentin.menoret@yahoo.fr>
2021-12-10 22:47:38 +0100

    Suppression robots.txt - modification style page presentation - modif appel favicon dans index.html

--- src/data/presentation.js
+++ src/data/presentation.js
@@ -1,16 +1,10 @@
 export default [
     {
-        contenu: "Coucou c'est moi Josepine Alfonse de la villardière",
-        styleText: {
-            'color': 'blue',
-            'font-family': 'Fasthand'
-        }
+        contenu: "Coucou c'est moi Josepine Alfonse de la villardière. J'aime les burger au veau",
+        styleText: {}
     },
     {
-        contenu: "Ta mère ! Le rap suspendu à nos coooouilles ! Clique sur chnake grosse canaille ! Gros pectoro à la Broly ! J'ouvre la puta de secoours !",
-        styleText: {
-            'color': 'violet',
-            'font-family': 'Anton, sans-serif'
-        }
+        contenu: "Pleein de texte qui dit des trucs lalalala lalala lalala cocuocu lalala allala coucou lalala lalal allaal lal lala lala",
+        styleText: {}
     }
 ]
